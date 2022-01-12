96fc836ba1e23b7caa6950d4618f4c21c0b2f57f
brightstudent <70577787+brightstudent@users.noreply.github.com>
2022-01-12 18:02:11 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
